2d9b6ee4035d29c0a166e707a0674aa12635bd51
Kurtis Rogers <krogers@KY72VR7X5X.local>
2023-03-29 14:09:10 +0100

    Got status com,ing through with the html link list item - Need to change the structure of the data coming back - extra array step is annoying

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(3001, () => {
 
 async function DataCollection() {
   const posts = await fetch(
-    "https://content.nomensa.com/blog?_start=0&_limit=400",
+    "https://content.nomensa.com/blog?_start=0&_limit=50",
     {
       method: "GET",
       withCredentials: true,
@@ -22,12 +22,77 @@ async function DataCollection() {
     }
   ).then((res) => res.json());
 
-  return posts.map(post => {
+  return await Promise.all(posts.map( async(post) => {
     return {
       id: post?.id,
       title: post?.title,
       slug: post?.slug,
-      slices: post?.slices,
+      links: await handleSlices(post?.slices)
     }
-  })
+  }))
+}
+
+async function handleSlices(slices) {
+  return await Promise.all(slices.map(async (slice) => {
+    if (slice.__component === "slices.textblock") {
+      return await handleFindContentLinks(slice.content)
+    }
+  }))
+}
+
+function handleFindContentLinks(content) {
+  const linkRegex = /href=['"](?<url>.*?)['"]|\[(.+)\]\(([^ ]+)( "(.+)")?\)/gm;
+  const links = content.match(linkRegex);
+  let linksArray = [];
+
+  const handleListGen = links && links.map(async (link) => {
+    const formattedLink = link
+    .replace('href="', "")
+    .replace(/^\[([\w\s\d]+)\]\((https?:\/\/[\w\d./?=#]+)\)$/gm, "$2")
+    .replace('"', "")
+    .replace(",", "");
+
+    if (
+      formattedLink.length > 10 &&
+      formattedLink.includes("nomensa.com") &&
+      !formattedLink.includes(".jpg") &&
+      !formattedLink.includes(".png")
+      ) {
+
+      const linkStatusCheckerData = await linkStatusChecker(formattedLink)
+
+      return linksArray.push({
+        link: `
+          <li class="link">
+            <a href="${formattedLink}" target="_blank">${
+          formattedLink.includes("javascript:")
+            ? "<span class='text-red-500'>Event: " + formattedLink + "</span>"
+            : formattedLink
+            }</a><span>${linkStatusCheckerData.status}</span>
+          </li>
+        `,
+        status: linkStatusCheckerData.status
+      });
+    }
+  });
+
+  
+  return links && Promise.allSettled(handleListGen).then(() => {
+    console.log(linksArray)
+
+    return linksArray
+  });
+};
+
+async function linkStatusChecker(url) {
+  if (!url || !url.length > 10) return
+  
+  try {
+    const response = await fetch(url)
+    const data = await response
+
+    return data
+  } catch (error) {
+    console.log(error);
+  }
 }
